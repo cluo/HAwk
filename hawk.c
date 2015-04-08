5ebf30424b3ab2f7e3c77b8bb97b938558b99bfc
Dylan F Marquis <dylanfmarquis@dylanfmarquis.com>
2015-04-08 13:33:04 -0400

    Update hawk.c

--- hawk.c
+++ hawk.c
@@ -1,7 +1,7 @@
 /*  HAwk - MySQL/MariaDB monitoring for HAproxy load balancing
     Copyright (C) 2014  
     
-    Author: Dylan F Marquis (dylanfmaquis@dylanfmarquis.com)
+    Author: Dylan F Marquis (dylanfmarquis@dylanfmarquis.com)
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
